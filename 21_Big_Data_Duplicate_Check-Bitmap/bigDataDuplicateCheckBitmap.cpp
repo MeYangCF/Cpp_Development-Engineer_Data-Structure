9586986557fc89b6a8b8247890fd93d098750a26
摆烂杨 <2971208523@qq.com>
2024-12-20 15:21:55 +0800

    更新大数据查重代码文件

--- 21_Big_Data_Duplicate_Check-Bitmap/bigDataDuplicateCheckBitmap.cpp
+++ 21_Big_Data_Duplicate_Check-Bitmap/bigDataDuplicateCheckBitmap.cpp
@@ -1,14 +1,36 @@
 /*
 @author: Yang ChengFeng
 @email: meyangcf@163.com
-@date: 2024 24-12-20 涓婂崍11:31
+@date: 2024 24-12-20 上午11:31
 @description:
 */
 
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <memory>
 using namespace std;
 
 int main(){
+    vector<int> vec{12, 78, 90, 78, 123, 8, 9, 90};
 
+    int max = vec[0];
+    for(int i = 1; i < vec.size(); i++)
+        if(vec[i] > max)
+            max = vec[i];
+
+    int* bitmap = new int[max / 32 + 1]();
+    unique_ptr<int> ptr(bitmap);
+
+    for(auto key : vec){
+        int index = key / 32;
+        int offset = key % 32;
+
+        if(0 == (bitmap[index] & (1 << offset)))
+            bitmap[index] |= (1 << offset);
+        else
+            cout << key << "重复出现过!" << endl;
+    }
     return 0;
 }
