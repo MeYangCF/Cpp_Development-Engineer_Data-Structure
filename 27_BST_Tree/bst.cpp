b4dbbb8a5582af9ef8c6f5388ad5c789ecb32512
摆烂杨 <2971208523@qq.com>
2024-12-30 16:22:57 +0800

    更新bst代码文件

--- 27_BST_Tree/bst.cpp
+++ 27_BST_Tree/bst.cpp
@@ -258,6 +258,55 @@ public:
         cout << endl;
     }
 
+    void findValues(vector<T>& vec, int i, int j) {
+        findValues(root_, vec, i, j);
+    }
+
+    bool isBSTree() {
+        Node* pre = nullptr;
+        return isBSTree(root_, pre);
+    }
+
+    bool isChildTree(BSTree<T, Comp>& child) {
+        if (child.root_ == nullptr)
+            return true;
+
+        Node* cur = root_;
+        while (cur != nullptr) {
+            if (cur->data_ == child.root_->data_)
+                break;
+            else if (comp_(cur->data_, child.root_->data_))
+                cur = cur->right_;
+            else
+                cur = cur->left_;
+        }
+
+        if (cur == nullptr)
+            return false;
+
+        return isChildTree(cur, child.root_);
+    }
+
+    int getLCA(int val1, int val2) {
+        Node* node = getLCA(root_, val1, val2);
+
+        if (node == nullptr)
+            throw "no LCA";
+        else
+            return node->data_;
+    }
+
+    void mirror01() {
+        mirror01()(root_);
+    }
+
+    bool mirror02() {
+        if (root_ == nullptr)
+            return true;
+
+        return mirror02(root_->left_, root_->right_);
+    }
+
 public:
     struct Node {
         Node(T data = T())
@@ -391,7 +440,167 @@ public:
         int right = number(node->right_);
         return left + right + 1;
     }
+
+    void findValues(Node* node, vector<T>& vec, int i, int j) {
+        if (node != nullptr) {
+            if (node->data_ > i)
+                findValues(node->left_, vec, i, j);
+            if (node->data_ >= i && node->data_ <= j)
+                vec.push_back(node->data_);
+            if (node->data_ < j)
+                findValues(node->right_, vec, i, j);
+        }
+    }
+
+    bool isBSTree(Node* node, Node*& pre) {
+        if (node == nullptr)
+            return true;
+
+        if (!isBSTree(node->left_, pre))
+            return false;
+
+        if (pre != nullptr) {
+            if (comp_(node->data_, pre->data_))
+                return false;
+        }
+
+        pre = node;
+        return isBSTree(node->right_, pre);
+    }
+
+    bool isChildTree(Node* father, Node* child) {
+        if (father == nullptr && child == nullptr)
+            return true;
+
+        if (father == nullptr)
+            return false;
+
+        if (child == nullptr)
+            return true;
+
+        if (father->data_ != child->data_)
+            return false;
+
+        return isChildTree(father->left_, child->left_)
+            && isChildTree(father->right_, child->right_);
+    }
+
+    Node* getLCA(Node* node, int val1, int val2) {
+        if (node == nullptr)
+            return nullptr;
+
+        if (comp_(node->data_, val1) && comp_(node->data_, val2))
+            return getLCA(node->right_, val1, val2);
+        else if (comp_(val1, node->data_) && comp_(val2, node->data_))
+            return getLCA(node->left_, val1, val2)
+        else
+            return node;
+    }
+
+    void mirror01(Node* node) {
+        if (node == nullptr)
+            return;
+
+        Node* tmp = node->left;
+        node->left_ = node->right_;
+        node->right_ = tmp;
+
+        mirror01(node->left_);
+        mirror01((node->right_));
+
+    }
+
+    bool mirror02(Node* node1, Node* node2) {
+        if (node1 == nullptr && node2 == nullptr)
+            return true;
+
+        if (node1 == nullptr || node2 == nullptr)
+            return false;
+
+        if (node1->data_ != node2->data_)
+            return false;
+
+        return mirror02(node1->left_, node2->right_)
+            && mirror02(node1->right_, node2->left_);
+    }
 };
+
+void test01() {
+    using Node = BSTree<int>::Node;
+    BSTree<int> bst;
+    bst.root_ = new Node(40);
+    Node* node1 = new Node(20);
+    Node* node2 = new Node(60);
+    Node* node3 = new Node(30);
+    Node* node4 = new Node(80);
+
+    bst.root_->left_ = node1;
+    bst.root_->right_ = node2;
+    node2->left_ = node3;
+    node2->right_ = node4;
+
+    cout << bst.isBSTree() << endl;
+}
+
+void test02() {
+    int ar[] = { 58,24,67,0,34,62,69,5,41,64,78 };
+    BSTree<int> bst;
+
+    for (int v : ar)
+        bst.insert(v);
+
+    using Node = BSTree<int>::Node;
+    BSTree<int> bst1;
+    bst1.root_ = new Node(67);
+    Node* node1 = new Node(62);
+    Node* node2 = new Node(69);
+    Node* node3 = new Node(60);
+    bst1.root_->left_ = node1;
+    bst1.root_->right_ = node2;
+    node1->left_ = node3;
+
+    cout << bst.isChildTree(bst1) << endl;
+}
+
+void test03() {
+    int ar[] = { 58,24,67,0,34,62,69,5,41,64,78 };
+    BSTree<int> bst;
+    for (int v : ar)
+
+        bst.insert(v);
+
+    cout << bst.getLCA(64, 62) << endl;
+}
+
+void test04() {
+    using Node = BSTree<int>::Node;
+    BSTree<int> bst;
+    bst.root_ = new Node(40);
+    Node* node1 = new Node(20);
+    Node* node2 = new Node(20);
+    Node* node3 = new Node(10);
+    Node* node4 = new Node(15);
+    Node* node5 = new Node(15);
+    Node* node6 = new Node(10);
+
+    bst.root_->left_ = node1;
+    bst.root_->right_ = node2;
+    node1->left_ = node3;
+    node1->right_ = node4;
+    node2->left_ = node5;
+    //node2->right_ = node6;
+
+    cout << bst.mirror02() << endl;
+}
+
+void test05() {
+    BSTree<int> bst;
+    int pre[] = { 58,24,0,5,34,41,67,62,64,69,78 };
+    int in[] = { 0,5,24,34,41,58,62,64,67,69,78 };
+    bst.rebuild(pre, 0, 10, in, 0, 10);
+    bst.preOrder();
+    bst.inOrder();
+}
 
 int main(){
 
