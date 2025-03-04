82851249162cfc4f25ce32d1306819c174a9fee1
摆烂杨 <2971208523@qq.com>
2025-03-04 10:57:32 +0800

    更新项目文件

--- 10_BinarySearchAlgorithm/binarySearch.cpp
+++ 10_BinarySearchAlgorithm/binarySearch.cpp
@@ -23,20 +23,20 @@ int BinarySearch(int arr[], int i, int j, int val){
 }
 
 //二分查找的非递归实现
-//int BinarySearch(int arr[], int size, int val){
-//    int left = 0;
-//    int right = size - 1;
-//    while (left <= right){
-//        int mid = (left + right) / 2;
-//        if(arr[mid] == val)
-//            return mid;
-//        else if(arr[mid] > val)
-//            right = mid - 1;
-//        else
-//            left = mid + 1;
-//    }
-//    return -1;
-//}
+// int BinarySearch(int arr[], int size, int val){
+//     int left = 0;
+//     int right = size - 1;
+//     while (left <= right){
+//         int mid = (left + right) / 2;
+//         if(arr[mid] == val)
+//             return mid;
+//         else if(arr[mid] > val)
+//             right = mid - 1;
+//         else
+//             left = mid + 1;
+//     }
+//     return -1;
+// }
 int main(){
     int arr[] = {12, 25, 34, 39, 45, 57, 63, 78, 82, 96, 100};
     int size = sizeof arr / sizeof arr[0];
